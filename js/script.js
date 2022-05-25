4e39ecdb47ee0ee8b3e2d8d6f81ecbfebff76263
muqtadi <abdullohmuqtadi@gmail.com>
2022-05-25 15:21:43 +0500

    Add Email section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const mainBtn = document.querySelector('.main__btn');
 const validMsg = document.querySelector('.valid__msg');
 const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const emailInput = document.querySelector('.email__input');
+const emailBtn = document.querySelector('.email__btn ');
+const emailError = document.querySelector('.email__error');
+
 
 
 
@@ -20,5 +24,25 @@ mainBtn.addEventListener('click', () =>{
   else{
     validMsg.style.display = 'block';
     input.value = '';
+  }
+})
+
+
+
+emailBtn.addEventListener('click', () =>{
+  if(emailInput.value === ''){
+    emailError.style.display = 'none';
+    emailInput.style = 'border: 2px solid rgb(166,107,135)';
+
+  }
+  if(emailInput.value.match(validRegex)){
+    console.log(emailInput.value);
+    emailInput.style = 'border: 1px solid )';
+    emailInput.value = '';
+    emailError.style.display = 'none';
+  }
+  else{
+    emailInput.style = 'border: 2px solid rgb(166,107,135)';
+    emailError.style.display = 'block';
   }
 })
